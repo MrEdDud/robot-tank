acf5a331a49b4572032a70e8d16bb2b16ed3f7b8
Eduard <eduardcojocaru2016@gmail.com>
2025-08-17 19:24:28 +0100

    Fixed Movement Logic

--- motor_driver.py
+++ motor_driver.py
@@ -1,65 +1,21 @@
-from gpiozero import Motor, PWMOutputDevice, DigitalOutputDevice
-import curses
+from gpiozero import Motor, DigitalOutputDevice
+import time
 
-# Standby
 STBY = DigitalOutputDevice(22)
 STBY.on()
 
-class Vehicle:
-    def __init__(self):
-        # Motor A
-        self.motor_a = Motor(forward=16, backward=18)
-        self.pwm_a = PWMOutputDevice(12)  # PWMA
-        self.pwm_a.value = 0
+# Motor A test
+motor_a = Motor(forward=16, backward=18)
+motor_a.forward()
+time.sleep(2)
+motor_a.backward()
+time.sleep(2)
+motor_a.stop()
 
-        # Motor B
-        self.motor_b = Motor(forward=15, backward=13)
-        self.pwm_b = PWMOutputDevice(11)  # PWMB
-        self.pwm_b.value = 0
-
-        self.speed = 0.5  # default speed
-
-    def forward(self):
-        self.motor_a.forward()
-        self.motor_b.forward()
-        self.pwm_a.value = self.speed
-        self.pwm_b.value = self.speed
-
-    def backward(self):
-        self.motor_a.backward()
-        self.motor_b.backward()
-        self.pwm_a.value = self.speed
-        self.pwm_b.value = self.speed
-
-    def stop(self):
-        self.motor_a.stop()
-        self.motor_b.stop()
-        self.pwm_a.value = 0
-        self.pwm_b.value = 0
-
-    def map_key_to_command(self, key):
-        return {
-            curses.KEY_UP: self.forward,
-            curses.KEY_DOWN: self.backward,
-        }.get(key)
-
-    def control(self, key):
-        action = self.map_key_to_command(key)
-        if action:
-            action()
-
-rpi_vehicle = Vehicle()
-
-def main(window):
-    while True:
-        curses.halfdelay(5)  # wait 0.5s per read
-        key = window.getch()
-
-        if key == 259:  # UP
-            rpi_vehicle.forward()
-        elif key == 258:  # DOWN
-            rpi_vehicle.backward()
-        else:
-            rpi_vehicle.stop()
-
-curses.wrapper(main)
+# Motor B test
+motor_b = Motor(forward=15, backward=13)
+motor_b.forward()
+time.sleep(2)
+motor_b.backward()
+time.sleep(2)
+motor_b.stop()
