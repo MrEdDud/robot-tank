c610b6e1d0fe4efcb29c46ab34f0c5341ceb4be1
Eduard <eduardcojocaru2016@gmail.com>
2025-08-17 19:43:40 +0100

    Fixed Movement Logic

--- motor_driver.py
+++ motor_driver.py
@@ -4,18 +4,27 @@ import time
 STBY = DigitalOutputDevice(22)
 STBY.on()
 
-# Motor A test
 motor_a = Motor(forward=16, backward=18)
+motor_b = Motor(forward=15, backward=13)
+
+# Test Motor A
+print("Motor A forward")
 motor_a.forward()
 time.sleep(2)
+motor_a.stop()
+
+print("Motor A backward")
 motor_a.backward()
 time.sleep(2)
 motor_a.stop()
 
-# Motor B test
-motor_b = Motor(forward=15, backward=13)
+# Test Motor B
+print("Motor B forward")
 motor_b.forward()
 time.sleep(2)
+motor_b.stop()
+
+print("Motor B backward")
 motor_b.backward()
 time.sleep(2)
 motor_b.stop()
