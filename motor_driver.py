260f4dffdb9c350ba8b38bdda189cda8f8d2ea35
Eduard <eduardcojocaru2016@gmail.com>
2025-08-18 18:12:47 +0100

    Fixed Movement Logic

--- motor_driver.py
+++ motor_driver.py
@@ -13,35 +13,35 @@ PWMB = PWMOutputDevice(11)      # Motor B speed (PWM)
 
 # --- Enable driver ---
 STBY.on()
+while True:
+    # --- Motor A forward ---
+    A1.on()
+    A2.off()
+    PWMA.value = 0.6   # 60% speed
+    time.sleep(2)
 
-# --- Motor A forward ---
-A1.on()
-A2.off()
-PWMA.value = 0.6   # 60% speed
-time.sleep(2)
+    # --- Motor A backward ---
+    A1.off()
+    A2.on()
+    PWMA.value = 0.6
+    time.sleep(2)
 
-# --- Motor A backward ---
-A1.off()
-A2.on()
-PWMA.value = 0.6
-time.sleep(2)
+    # --- Motor B forward ---
+    B1.on()
+    B2.off()
+    PWMB.value = 0.6
+    time.sleep(2)
 
-# --- Motor B forward ---
-B1.on()
-B2.off()
-PWMB.value = 0.6
-time.sleep(2)
+    # --- Motor B backward ---
+    B1.off()
+    B2.on()
+    PWMB.value = 0.6
+    time.sleep(2)
 
-# --- Motor B backward ---
-B1.off()
-B2.on()
-PWMB.value = 0.6
-time.sleep(2)
-
-# --- Stop everything ---
-A1.off()
-A2.off()
-PWMA.value = 0
+    # --- Stop everything ---
+    A1.off()
+    A2.off()
+    PWMA.value = 0
 
 B1.off()
 B2.off()
