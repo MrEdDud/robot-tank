ed760f75c5fb6030589b2ca5f950a377cadd8978
Eduard <eduardcojocaru2016@gmail.com>
2026-01-10 21:23:20 +0000

    fixed launcher script again

--- launcher.sh
+++ launcher.sh
@@ -1,17 +1,18 @@
-#!/bin/bash
+#!/usr/bin/bash
 echo "Starting motor server..."
 python3 motor_server.py &
-
-echo "Activating virtual environment..."
-source ~/Desktop/robot-tank/.venv/bin/activate
-
-echo "Starting ROBOT TANK..."
-python3 api.py
+MOTOR_PID=$!
 
 cleanup() {
     echo "Shutting down motor server..."
     kill $MOTOR_PID
 }
 trap cleanup EXIT
+
+echo "Activating virtual environment..."
+source ~/Desktop/robot-tank/.venv/bin/activate
+
+echo "Starting ROBOT TANK..."
+python3 api.py
 
 wait
